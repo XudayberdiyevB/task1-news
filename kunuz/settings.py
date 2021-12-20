6bafe139270aa1330e5d357af60baaeb41db9b27
Bekzod <63925033+XudayberdiyevB@users.noreply.github.com>
2021-12-20 15:26:10 +0500

    changes

--- kunuz/settings.py
+++ kunuz/settings.py
@@ -10,8 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_heroku
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # from django.conf import settings
 # settings.configure(DEBUG=True)
@@ -24,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8uv-t4svv#sv%q0r$&kpbrfw%khuc4m(x2fwj@#7$s-(x+&izq'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
